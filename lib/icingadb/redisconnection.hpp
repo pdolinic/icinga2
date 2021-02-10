ed72f3a649ed9fb7331d0386cfdce98f6dd4223f
Alexander A. Klimov <alexander.klimov@icinga.com>
2021-02-10 16:54:07 +0100

    RedisConnection: log actual query performance

--- lib/icingadb/redisconnection.hpp
+++ lib/icingadb/redisconnection.hpp
@@ -7,11 +7,13 @@
 #include "base/atomic.hpp"
 #include "base/io-engine.hpp"
 #include "base/object.hpp"
+#include "base/ringbuffer.hpp"
 #include "base/shared.hpp"
 #include "base/string.hpp"
 #include "base/value.hpp"
 #include <boost/asio/buffer.hpp>
 #include <boost/asio/buffered_stream.hpp>
+#include <boost/asio/deadline_timer.hpp>
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/io_context_strand.hpp>
 #include <boost/asio/ip/tcp.hpp>
@@ -147,6 +149,7 @@ namespace icinga
 		void Connect(boost::asio::yield_context& yc);
 		void ReadLoop(boost::asio::yield_context& yc);
 		void WriteLoop(boost::asio::yield_context& yc);
+		void LogStats(boost::asio::yield_context& yc);
 		void WriteItem(boost::asio::yield_context& yc, WriteQueueItem item);
 		Reply ReadOne(boost::asio::yield_context& yc);
 		void WriteOne(Query& query, boost::asio::yield_context& yc);
@@ -156,6 +159,9 @@ namespace icinga
 
 		template<class StreamPtr>
 		void WriteOne(StreamPtr& stream, Query& query, boost::asio::yield_context& yc);
+
+		void IncreasePendingQueries(int count);
+		void DecreasePendingQueries(int count);
 
 		String m_Path;
 		String m_Host;
@@ -186,6 +192,12 @@ namespace icinga
 		AsioConditionVariable m_QueuedWrites, m_QueuedReads;
 
 		std::function<void(boost::asio::yield_context& yc)> m_ConnectedCallback;
+
+		// Stats
+		RingBuffer m_InputQueries{10};
+		RingBuffer m_OutputQueries{10};
+		uint_fast64_t m_PendingQueries{0};
+		boost::asio::deadline_timer m_LogStatsTimer;
 	};
 
 /**
